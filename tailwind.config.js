4055c29ba76a4254710927aa3a064dafc9782503
aryatyagi8800 <aryatyagi8800@gmail.com>
2025-01-20 12:56:59 +0530

    animations-breakpoints-darkModes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,28 @@
 module.exports = {
   content: ["*"],
   theme: {
-    extend: {},
+    extend: {
+    //   screens:{
+    //   md:'800px',
+    //   lg:'1100px'
+    // },
+    //It changes for all padding,margin,height,width
+    spacing:{
+      '21':'6rem',
+      'px-8':'8px',
+      '31':'300px'
+    },
+    // padding:{
+    //   '21':'6rem',
+    //   'px-8':'8px'
+    // },
+    // margin:{
+    //   '21':'6rem'
+    // },
+    // height:{
+    //   '31':'300px'
+    // }
+    },
     container:{
       center:true
     }
